3d2d8f8e5eccab6f6e71883387dfbc51525424c6
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-02-07 21:49:41 +0430

    Correct the carts background color.

--- Script.js
+++ Script.js
@@ -1,9 +1,11 @@
 const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.nav-menu');
+const navBranding = document.querySelector('.nav-branding');
 
 humburger.addEventListener('click', () => {
   humburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  navBranding.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-link').forEach((links) => links.addEventListener('click', () => {
