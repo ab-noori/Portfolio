809ffa4bc91bf99c1b082b4f54ab771064855154
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-02-07 19:45:48 +0430

    Fix the issue with disapearing the mobile menu.

--- Script.js
+++ Script.js
@@ -1,15 +1,12 @@
 const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.nav-menu');
-const navlink=
+
 humburger.addEventListener('click', () => {
   humburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach(links =>links.addEventListener('click', ()=>{
+document.querySelectorAll('.nav-link').forEach((links) => links.addEventListener('click', () => {
   humburger.classList.remove('active');
   navMenu.classList.remove('active');
-}))
-   
-
-
+}));
