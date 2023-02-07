ce61caa7d186d5fb778e73eb56dbacbe34002c9c
Munana Clinton <munanaclinton@gmail.com>
2023-02-07 15:21:59 +0200

    Add changes to the menu-content on script file

--- Script.js
+++ Script.js
@@ -1,7 +1,12 @@
 const humburger = document.querySelector('.humburger');
 const navMenu = document.querySelector('.nav-menu');
-
+const navlink=document.querySelector('.nav-item')
 humburger.addEventListener('click', () => {
   humburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+navlink.addEventListener ('click', () =>{
+   navMenu.style.display = 'none';
+   
+
+})
